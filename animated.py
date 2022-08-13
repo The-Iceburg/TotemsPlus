483d76bf8215780fad52e07c2fbb8e3e4bec7d6e
Ormatist <thescientist771.mail@gmail.com>
2022-08-13 18:27:46 +0100

    v1.1.3
    Basic feature-set has been implemented in the ANIM function. Integration still needs to occur in both OFCIT & MCCMD

--- animated.py
+++ animated.py
@@ -6,10 +6,12 @@
 ###################################################################
 
 # imports the libaries used within totems+
+from email.utils import format_datetime
 from PIL import Image
+import os, shutil, getpass
 
 # defines the texture convereter subroutine
-def animatedConverter(imageLocation):
+def ANIM(imageLocation, packName, integrationType, rename):
 
     # opens the image as an object python/PIL can interact with
     imageObject = Image.open(imageLocation)
@@ -37,8 +39,46 @@ def animatedConverter(imageLocation):
         # pastes the image using the calculated buffer and height
         new.paste(imageObject, (-buffer, height2))
 
+     # if the directory resized already exists, then delete it
+    if os.path.exists('C:/Users/' + getpass.getuser() + '/AppData/Roaming/Totems+/giftexture'):
+        shutil.rmtree('C:/Users/' + getpass.getuser() + '/AppData/Roaming/Totems+/giftexture')
+
+    # make the resized folder
+    os.mkdir("C:/Users/" + getpass.getuser() + "/AppData/Roaming/Totems+/giftexture")
+
+    locationList = imageLocation.split("/")
+
     # saves the new image
-    new.save("img/test2.png")
+    new.save("C:/Users/" + getpass.getuser() + "/AppData/Roaming/Totems+/giftexture/" + locationList[-1])
+
+    # sets frametime to a temporary constant (it will be variable in the future)
+    FRAMETIME = 1
+
+    if integrationType == "MCCMD":
+
+        file = open("C:/Users/" + getpass.getuser() + "/AppData/Roaming/.minecraft/resourcepacks/" + packName + "/assets/minecraft/textures/totems/" + locationList[-1] + ".mcmeta", "w+")
+
+        file.writelines(['{\n',
+        '  "animation": {\n'])
+        file.write('    "frametime": ' + FRAMETIME + '\n')
+        file.writelines(['  }\n',
+        '}'])
+
+        file.close()
+
+    elif integrationType == "OFCIT":
+
+        file = open("C:/Users/" + getpass.getuser() + "/AppData/Roaming/.minecraft/resourcepacks/" + packName + "/assets/minecraft/optifine/cit/totems/" + str(rename.lower()) + "/" + locationList[-1] + ".mcmeta", "w+")
+
+        file.writelines(['{\n',
+        '  "animation": {\n'])
+        file.write('    "frametime": ' + FRAMETIME + '\n')
+        file.writelines(['  }\n',
+        '}'])
+
+        file.close()
+
+    return "C:/Users/" + getpass.getuser() + "/AppData/Roaming/Totems+/giftexture" + locationList[-1]
 
 # runs the subroutine for testing purposes
-animatedConverter("img/test.gif")
+ANIM("img/test.gif")
