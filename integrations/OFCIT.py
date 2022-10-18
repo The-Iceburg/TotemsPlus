2ed6c6151f4e75223f85ec0dad1544c8f558ca3c
Ormatist <thescientist771.mail@gmail.com>
2022-10-18 19:59:01 +0100

    v1.3.6
    refactored and jsonified (I'm pioneering that term) OFCIT, all relevant files coming before pull req!

--- integrations/OFCIT.py
+++ integrations/OFCIT.py
@@ -6,14 +6,19 @@
 ###################################################################
 
 # imports the libaries used within Totems+ 
-import os, shutil, getpass, PySimpleGUI as sg
+import os, shutil, getpass, PySimpleGUI as sg, json
 from addons.ANIM import ANI
 from addons.RESZ import RES
 
 # outlines the versions and there pack formats
-packFormat4 = ["1.14","1.14.1","1.14.2","1.14.3","1.14.4"]
-packFormat5 = ["1.15","1.15.1","1.15.2","1.16","1.16.1"]
-packFormat6 = ["1.16.2","1.16.3","1.16.4","1.16.5"]
+packFormatCodes = {"1.14": 4, "1.14.1": 4, "1.14.2": 4,"1.14.3": 4,"1.14.4": 4,
+                   "1.15": 5, "1.15.1": 5,"1.15.2": 5, "1.16": 5,"1.16.1": 5,
+                   "1.16.2": 6, "1.16.3": 6, "1.16.4": 6, "1.16.5": 6,
+                   "1.17": 7, "1.17.1": 7,
+                   "1.18": 8, "1.18.1": 8, "1.18.2": 8,
+                   "1.19": 9, "1.19.1": 9, "1.19.2": 9}
+
+packMeta = {"pack" : {"pack_format": 0, "description": ""}}
 
 # defines the CIT function
 def CIT(textureList, version):
@@ -69,47 +74,24 @@ def CIT(textureList, version):
             if name == dirList[i]:
                 name = sg.popup_get_text("An OFCIT Integration resource pack already exists\nPlease choose a different name for this one:", title = "Duplicate Pack")
                 break
+
         # else if the name is unique, end the loop
         else:
             repeatLoop = False
 
     # pre-makes the resource pack directory in the minecraft resource pack folder
-    os.mkdir("C:/Users/" + getpass.getuser() + "/AppData/Roaming/.minecraft/resourcepacks/" + name)
-    os.mkdir("C:/Users/" + getpass.getuser() + "/AppData/Roaming/.minecraft/resourcepacks/" + name + "/assets")
-    os.mkdir("C:/Users/" + getpass.getuser() + "/AppData/Roaming/.minecraft/resourcepacks/" + name + "/assets/minecraft")
-    os.mkdir("C:/Users/" + getpass.getuser() + "/AppData/Roaming/.minecraft/resourcepacks/" + name + "/assets/minecraft/optifine")
-    os.mkdir("C:/Users/" + getpass.getuser() + "/AppData/Roaming/.minecraft/resourcepacks/" + name + "/assets/minecraft/optifine/cit")
-    os.mkdir("C:/Users/" + getpass.getuser() + "/AppData/Roaming/.minecraft/resourcepacks/" + name + "/assets/minecraft/optifine/cit/totems")
+    os.makedirs("C:/Users/" + getpass.getuser() + "/AppData/Roaming/.minecraft/resourcepacks/" + name + "/assets/minecraft/optifine/cit/totems")
 
     # copys the pack.png file into place
     shutil.copy("img/pack.png", "C:/Users/" + getpass.getuser() + "/AppData/Roaming/.minecraft/resourcepacks/" + name)
 
     # creates & opens the pack.mcmeta file
-    packMeta = open("C:/Users/" + getpass.getuser() + "/AppData/Roaming/.minecraft/resourcepacks/" + name + "/pack.mcmeta", "w+")
+    with open("C:/Users/" + getpass.getuser() + "/AppData/Roaming/.minecraft/resourcepacks/" + name + "/pack.mcmeta", "w+") as packMetaFile:
 
-    # derrives the resource pack format code from the version
-    if version in packFormat4:
-        packFormat = 4
-    elif version in packFormat5:
-        packFormat = 5
-    elif version in packFormat6:
-        packFormat = 6
-    elif version.startswith("1.17"):
-        packFormat = 7
-    elif version.startswith("1.18"):
-        packFormat = 8
-    elif version.startswith("1.19"):
-        packFormat = 9
-
-    # adds the needed meta data to the pack.mcmeta file
-    packMeta.writelines(['{\n',
-    '  "pack": {\n',
-    '    "pack_format": ' + str(packFormat) + ',\n',
-    '	"description": "Version: ' + version + '\n',
-    'Made By: The Totems+ Team"\n',
-    '  }\n',
-    '}'])
-    packMeta.close()
+        # adds the needed meta data to the pack.mcmeta file
+        packMeta["pack"]["pack_format"] = packFormatCodes[version]
+        packMeta["pack"]["description"] = (f"Version: {version}\nMade By: The Totems+ Team")
+        packMetaFile.write(json.dumps(packMeta))
 
     # while window (GUI) is open
     while True:
