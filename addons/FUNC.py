9ea390f00d96bcf70d8f4060bab4fdd1e03dbfbc
Ormatist <thescientist771.mail@gmail.com>
2022-10-20 16:58:34 +0100

    v1.3.8
    refactored DOC now uses with statements :sunglasses:

--- addons/FUNC.py
+++ addons/FUNC.py
@@ -12,63 +12,48 @@ import os
 def FUN(worldLocation, nameList, inGameName, inGameLore, loreList):
 
     # creates the function directory
-    os.mkdir(worldLocation + "/datapacks/Totems+ CMD/data/totemsplus")
-    os.mkdir(worldLocation + "/datapacks/Totems+ CMD/data/totemsplus/functions")
+    os.makedirs(worldLocation + "/datapacks/Totems+ CMD/data/totemsplus/functions")
 
     # creates the totem load function
-    mcfunction = open(worldLocation + '/datapacks/Totems+ CMD/data/totemsplus/functions/totemload.mcfunction', 'w+')
+    with open(worldLocation + '/datapacks/Totems+ CMD/data/totemsplus/functions/totemload.mcfunction', 'w+') as mcfunction:
 
-    # cycles throught the name list
-    for i in range(len(nameList)):
+        # cycles throught the name list
+        for i in range(len(nameList)):
 
-        # writes univerasal start to command
-        mcfunction.write('give @s minecraft:totem_of_undying{')
-        
-        # writes the rest of the command dependent on if the in-game box was checked for lore or name
-        if inGameName[i] == True and loreList[i] != None:
+            # writes univerasal start to command
+            mcfunction.write('give @s minecraft:totem_of_undying{')
 
-            mcfunction.write('''display:{Name:'[{"text":"''' + nameList[i] + '''"}]',Lore:['[{"text":"''' + loreList[i]+ '''"}]']},CustomModelData:''' + str(910340 + i) +'} 1\n')
+            # writes the rest of the command dependent on if the in-game box was checked for name and if lore has a vaue
+            if inGameName[i] == True and loreList[i] != None:
+                mcfunction.write('''display:{Name:'[{"text":"''' + nameList[i] + '''"}]',Lore:['[{"text":"''' + loreList[i]+ '''"}]']},CustomModelData:''' + str(910340 + i) +'} 1\n')
 
-        elif inGameName[i] == True:
+            elif inGameName[i] == True:
+                mcfunction.write('''display:{Name:'[{"text":"''' + nameList[i] + '''"}]'},CustomModelData:''' + str(910340 + i) +'} 1\n')
 
-            mcfunction.write('''display:{Name:'[{"text":"''' + nameList[i] + '''"}]'},CustomModelData:''' + str(910340 + i) +'} 1\n')
+            elif loreList[i] != None:
+                mcfunction.write('''display:{Lore:['[{"text":"''' + loreList[i] + '''"}]']},CustomModelData:''' + str(910340 + i) +'} 1\n')
 
-        elif loreList[i] != None:
-
-            mcfunction.write('''display:{Lore:['[{"text":"''' + loreList[i] + '''"}]']},CustomModelData:''' + str(910340 + i) +'} 1\n')
-
-        else:
-
-            mcfunction.write('CustomModelData:' + str(910340 + i) +'} 1\n')
-
-    # closes the file
-    mcfunction.close()
+            else:
+                mcfunction.write('CustomModelData:' + str(910340 + i) +'} 1\n')
 
     # cycles throught the name list
     for i in range(len(nameList)):
 
         # creates the totems own file
-        mcfunction = open(worldLocation + '/datapacks/Totems+ CMD/data/totemsplus/functions/summon' + nameList[i].lower().replace(" ","_") + '.mcfunction', 'w+')
+        with open(worldLocation + '/datapacks/Totems+ CMD/data/totemsplus/functions/summon' + nameList[i].lower().replace(" ","_") + '.mcfunction', 'w+') as mcfunction:
 
-        # writes the universal info
-        mcfunction.write('give @s minecraft:totem_of_undying{')
-        
-        # writes the rest of the command dependent on if the in-game box was checked for lore or name
-        if inGameName[i] == "True" and inGameLore[i] == "True":
+            # writes the universal info
+            mcfunction.write('give @s minecraft:totem_of_undying{')
 
-            mcfunction.write('''display:{Name:'[{"text":"''' + nameList[i] + '''"}]',Lore:['[{"text":"''' + loreList[i]+ '''"}]']},CustomModelData:''' + str(910340 + i) +'} 1\n')
+            # writes the rest of the command dependent on if the in-game box was checked for name and if lore has a value
+            if inGameName[i] == "True" and inGameLore[i] == "True":
+                mcfunction.write('''display:{Name:'[{"text":"''' + nameList[i] + '''"}]',Lore:['[{"text":"''' + loreList[i]+ '''"}]']},CustomModelData:''' + str(910340 + i) +'} 1\n')
 
-        elif inGameName[i] == "True":
+            elif inGameName[i] == "True":
+                mcfunction.write('''display:{Name:'[{"text":"''' + nameList[i] + '''"}]'},CustomModelData:''' + str(910340 + i) +'} 1\n')
 
-            mcfunction.write('''display:{Name:'[{"text":"''' + nameList[i] + '''"}]'},CustomModelData:''' + str(910340 + i) +'} 1\n')
+            elif inGameLore[i] == "True":
+                mcfunction.write('''display:{Lore:['[{"text":"''' + loreList[i] + '''"}]']},CustomModelData:''' + str(910340 + i) +'} 1\n')
 
-        elif inGameLore[i] == "True":
-
-            mcfunction.write('''display:{Lore:['[{"text":"''' + loreList[i] + '''"}]']},CustomModelData:''' + str(910340 + i) +'} 1\n')
-
-        else:
-
-            mcfunction.write('CustomModelData:' + str(910340 + i) +'} 1\n')
-
-        # closes / saves the file
-        mcfunction.close()
+            else:
+                mcfunction.write('CustomModelData:' + str(910340 + i) +'} 1\n')
